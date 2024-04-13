e440e3b620b8add27c8f1553edd632abea05506a
Tskhvedadze <giorgicxvedadze4@gmail.com>
2024-04-13 04:12:30 +0400

    add carousel primary, split sections

--- src/assets/data/index.ts
+++ src/assets/data/index.ts
@@ -2,5 +2,12 @@ import headerData from "../data/header/headerData.json";
 import allNewsData from "../data/allNews/allNewsData.json";
 import asideListData from "../data/asideList/AsideListData.json";
 import asideListPopularData from "../data/asideList/asideListPopularData.json";
+import carouselData from "../data/carousel/carouselData.json";
 
-export { headerData, allNewsData, asideListData, asideListPopularData };
+export {
+  headerData,
+  allNewsData,
+  asideListData,
+  asideListPopularData,
+  carouselData,
+};
